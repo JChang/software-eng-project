77d4d2b25b0b7732110068a1a007bf744100c855
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-30 07:17:58 -0500

    docs/comments

--- Assets/_Scripts/DebugActions.cs
+++ Assets/_Scripts/DebugActions.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using  UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class DebugActions : MonoBehaviour
 {
+    /// <summary>
+    ///     Reload the current scene. Useful for testing new maze generation
+    /// </summary>
     public void ReloadScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    /// <summary>
+    ///     Add an arbitrary amount of points to the game
+    /// </summary>
+    /// <param name="amount">(integer) number of points to add. Defaults to 10.</param>
+    public void AddScore(int amount = 10)
+    {
+        GameManager.Instance.AddScore(amount);
+    }
+
+    /// <summary>
+    ///     Increase/decrease the health of the player.
+    /// </summary>
+    /// <param name="amount">The amount of health to add. Supports negative values.</param>
+    public void AddHealth(int amount = 3)
+    {
+        GameManager.Instance.DecreaseHealth(-amount);
     }
 }
