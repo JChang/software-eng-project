b86042495bca73ad2ed306fa622af6651604dd09
ToaMataNui1 <103005437+ToaMataNui1@users.noreply.github.com>
2024-12-01 14:47:18 -0500

    began work on game over screen

---
+++ Assets/_Scripts/EndGame.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject endGameScreen; 
+    private bool isGameOver = false;
+
+    void Update()
+    {
+        if (!isGameOver && GameManager.Instance.Health <= 0)
+        {
+            TriggerEndGame();
+        }
+    }
+
+    private void TriggerEndGame()
+    {
+        isGameOver = true;
+        endGameScreen.SetActive(true); 
+        Time.timeScale = 0f; 
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+    }
+
+    public void ExitToMainMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu"); 
+}
