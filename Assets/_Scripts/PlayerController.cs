4a8337a5065c2c1c52435abf275fbbfe494d9f27
Jin Chang <jinchang223@gmail.com>
2024-11-30 21:05:25 -0500

    Add health decreasing and immunity frames

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,9 +9,25 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Collider2D playerCollider;
+    
+    private ContactFilter2D contactFilter;
+
+    private SpriteRenderer playerSprite;
+
+    private int iframes = 0;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+
+        contactFilter = new ContactFilter2D();
+        contactFilter.useTriggers = true;
+        contactFilter.SetLayerMask(LayerMask.GetMask("Default"));
+        contactFilter.useLayerMask = true;
+
     }
 
     void Update()
@@ -30,6 +46,19 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if (touchingCreature() && iframes == 0){
+            GameManager.Instance.DecreaseHealth(1);
+            iframes = 1000;
+        }
+
+        if (iframes > 0){
+            iframes--;
+            playerSprite.color = Color.red;
+        }
+        else{
+            playerSprite.color = Color.white;
+        }
     }
 
     private void FixedUpdate()
@@ -44,6 +73,18 @@ public class PlayerController : MonoBehaviour
         transform.localScale = currentScale;
 
         _facingRight = !_facingRight;
+    }
+
+    private bool touchingCreature(){
+        Collider2D[] allCollisions = new Collider2D[10];
+        int count = playerCollider.OverlapCollider(contactFilter, allCollisions);
+
+        for (int i = 0; i< count; i++){
+            if (allCollisions[i].CompareTag("creature")){
+                return true;
+            }
+        }
+        return false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
